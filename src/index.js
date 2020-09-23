af1d234fbb0772e1c5a370e88de066a826f69f6b
github username <github email address>
2020-09-22 23:23:31 -0400

    added design

--- src/index.js
+++ src/index.js
@@ -7,3 +7,43 @@ console.log("Welcome!")
 //calls different methods like fetch and load deeds 
 //makes a call to backend API 
 //object of a  class that we are going to create a new instance of 
+
+
+
+
+// Your code here
+let dodger = document.getElementById('dodger');
+
+
+//move dodger left
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+//move dodger right 
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+//making sure the dodger can't move past the left edge 
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+//dodger can't move past right edge 
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
+
