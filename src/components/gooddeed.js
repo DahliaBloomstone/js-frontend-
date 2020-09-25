2097266d5e890ab1b82d548b0d7a160deff0001d
github username <github email address>
2020-09-24 20:09:45 -0400

    fixed error in patch request

--- src/components/gooddeed.js
+++ src/components/gooddeed.js
@@ -12,20 +12,7 @@ class Gooddeed { //create a new instance as we iterate over deed json array
   }
 }
   
-    /*
-    renderShow() {
-      return `<h3>${this.body}</h3>`
-    }
-  
-    render() {
-      return `<li data-gooddeedid='${this.id}' data-props='${JSON.stringify(
-        this
-      )}' class='gooddeed-element'><a class="show-link" href='#'>${
-        this.body
-      }</a> <button data-action='edit-gooddeed'>Edit</button> <i data-action='delete-gooddeed' class="em em-scream_cat"></i></li>`
-   
-    }
- */
+
  
  
   
